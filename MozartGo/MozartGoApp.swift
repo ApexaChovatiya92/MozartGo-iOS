fdddb13acc581453bada39f94fc21093d5e87c02
ApexaChovatiya92 <apexachovatiya@gmailcom>
2026-04-09 12:11:18 +0530

    Initial Commit

---
+++ MozartGo/MozartGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozartGoApp.swift
+//  MozartGo
+//
+//  Created by apexa Chovatiya on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MozartGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MozartGoTests/MozartGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MozartGoTests.swift
+//  MozartGoTests
+//
+//  Created by apexa Chovatiya on 09/04/26.
+//
+
+import Testing
+@testable import MozartGo
+
+struct MozartGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
